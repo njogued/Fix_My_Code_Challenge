f81b5966a408d8dad4a9406ce0098399f18b52d0
njogued <njogued@gmail.com>
2023-02-22 01:07:48 -0800

    Delete DLL node

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -46,8 +46,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		(*head)->prev->next = (*head)->next;
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
+        free(*head);
 		*head = saved_head;
-		free(*head);
 	}
 	return (1);
 }
